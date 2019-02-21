c2a614c48d21d564d2107f83463a217dbb8fb253
Pavel Bodrov <Max@Pasha.fritz.box>
2019-02-21 23:40:09 +0100

    solved second task(function pow(m,n))

--- 1-module/1-task/index.js
+++ 1-module/1-task/index.js
@@ -5,5 +5,13 @@
  * @returns {number}
  */
 function pow (m, n) {
+    let result = 1;
 
+    if(n<1 || n%1!==0) return;
+    
+    for(let i = 0; i<n; i++){
+        result *= m;
+    }
+
+    return result;
 }
