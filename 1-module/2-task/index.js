f412ab8da42f3007787acf1b964135a7971ec4d8
Pavel Bodrov <Max@Pasha.fritz.box>
2019-02-21 23:55:46 +0100

    solved task valid name

--- 1-module/2-task/index.js
+++ 1-module/2-task/index.js
@@ -12,6 +12,9 @@ function print (text) {
  * чтобы функция sayHello работала корректно
  */
 function isValid (name) {
+    if(!name || name.length<4 || ~name.indexOf(' ')) return false;
+    
+    return true;
 }
 
 function sayHello () {
