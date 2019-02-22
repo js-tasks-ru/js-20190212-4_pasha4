04cc2428dd60cc21ee676123ccad9fac2cfdd054
Pavel Bodrov <Max@Pasha.fritz.box>
2019-02-22 01:58:33 +0100

    solved task clone(obj)

--- 2-module/1-task/index.js
+++ 2-module/1-task/index.js
@@ -3,5 +3,16 @@
  * @param {Object} obj - клонируем объект
  * @returns {Object}
  */
-function clone (obj) {
+function clone(obj) {
+    let result = {};
+
+    for(let key in obj){
+        if(typeof obj[key] === 'object' && obj[key] !== null){
+            result[key] = clone(obj[key]);
+        } else {
+            result[key] = obj[key];
+        }
+    }
+    
+    return result;
 }
