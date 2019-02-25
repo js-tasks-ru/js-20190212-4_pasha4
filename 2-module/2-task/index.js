b7cacbcc6c511a86d082dacc7a444e83815d5829
Pavel Bodrov <Max@Pasha.fritz.box>
2019-02-26 00:16:32 +0100

    solved task 2.2 deep search for keys value

--- 2-module/2-task/index.js
+++ 2-module/2-task/index.js
@@ -5,4 +5,38 @@
  * @returns {Object}
  */
 function find (obj, value) {
+    let result = [];
+    let tmp = [];
+    let path = '';
+
+    for (let key in obj) {
+        if(typeof obj[key] === 'object'){
+            path += key + '.';
+            tmp.push(find(obj[key], value));
+
+            if(tmp.length<=1){
+                path += tmp;
+                result.push(path);
+                path = '';
+                tmp = [];
+            } else {
+                tmp.forEach(key => {
+                    path += key;
+                    result.push(path);
+                })
+            }
+        } else if(obj[key] === value){
+            path += key;
+            result.push(path);
+            path = '';
+        }
+    }
+
+    result = result.filter(elem => {
+        return elem.charAt(elem.length-1) !== '.' && elem;
+    });
+
+    if(result.length === 0) return null;
+
+    return result.length === 1 ? result[0] : result;
 }
